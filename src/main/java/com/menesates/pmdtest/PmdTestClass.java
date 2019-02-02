52f1a1a13fd22fc549c9a327c5407220df0a0f2e
Muhammed Enes Ates <atesmuhammedenes@gmail.com>
2019-02-02 21:41:17 +0300

    first commit

---
+++ src/main/java/com/menesates/pmdtest/PmdTestClass.java
@@ -0,0 +1,22 @@
+package com.menesates.pmdtest;
+
+public class PmdTestClass {
+
+    private int a = 5;
+    private int b = 15;
+
+    public void pmdTestMethod(){
+        System.out.println(a);
+    }
+
+    private void pmdTestMethod2(){
+
+    }
+
+    private void pmdTestMethod3(){
+        while (true){
+
+        }
+    }
+
+}
---
+++ src/test/java/com/menesates/jenkinstest/JenkinsTest.java
@@ -0,0 +1,13 @@
+package com.menesates.jenkinstest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JenkinsTest {
+
+    @Test
+    public void succesTest(){
+        Assert.assertEquals(2,2);
+    }
+
+}
